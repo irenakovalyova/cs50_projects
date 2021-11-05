f8d8c50ec325695db64c7b8c78f9130a08213f89
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-05 10:15:30 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
+int main(void)
+{
+// Getting the card number from the user    
     long card = get_long("What is the card number? ");
     printf("%li\n", card);
 
@@ -13,11 +15,14 @@ int main(void) {
     int i = 0;
 
 // Counting the sum of the first group of digits multiplied by 2 (in two stages)
-    for (i = 0; card_temp != 0; i++) {
-        if (i % 2 != 0) {
+    for (i = 0; card_temp != 0; i++) 
+    {
+        if (i % 2 != 0) 
+        {
             int product_digit_sum = 0;
             int product = (card_temp % 10) * 2;
-            for (int j = 0; product != 0; j++) {
+            for (int j = 0; product != 0; j++) 
+            {
                 product_digit_sum += product % 10;
                 product /= 10;
             }
@@ -30,8 +35,10 @@ int main(void) {
     i = 0;
 
 // Counting the sum of the secound group of digits
-    for (i = 0; card_temp != 0; i++) {
-        if (i % 2 == 0) {
+    for (i = 0; card_temp != 0; i++) 
+    {
+        if (i % 2 == 0) 
+        {
             total_sum_even += card_temp % 10;
         }
         card_temp /= 10;
@@ -43,15 +50,24 @@ int main(void) {
     int start_numbers = card / (pow(10, (i - 2)));
 
 // Determining the credit card type
-    if (luhn_num % 10 != 0) {
+    if (luhn_num % 10 != 0) 
+    {
         printf("%s\n", "INVALID");
-    } else if ((luhn_num % 10 == 0) && (i == 15) && (start_numbers == 34 || start_numbers == 37)) {
+    } 
+    else if ((luhn_num % 10 == 0) && (i == 15) && (start_numbers == 34 || start_numbers == 37)) 
+    {
         printf("%s\n", "AMEX");
-    } else if ((luhn_num % 10 == 0) && (i == 16) && (start_numbers >= 51) && (start_numbers <= 55)) {
+    } 
+    else if ((luhn_num % 10 == 0) && (i == 16) && (start_numbers >= 51) && (start_numbers <= 55)) 
+    {
         printf("%s\n", "MASTERCARD");
-    } else if ((luhn_num % 10 == 0) && (i == 16 || i == 13) && (start_numbers / 10 == 4)) {
+    } 
+    else if ((luhn_num % 10 == 0) && (i == 16 || i == 13) && (start_numbers / 10 == 4)) 
+    {
         printf("%s\n", "VISA");
-    } else {
+    } 
+    else 
+    {
         printf("%s\n", "INVALID");
     }
 }
