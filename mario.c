03dd7444ae2dca564edd1fa86631767a29290e23
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-05 10:21:25 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -3,40 +3,40 @@
 
 int main(void)
 {
-  int height = 0;
-  do
-  {
-    height = get_int("Height: ");
-  }
-  while (height < 1 || height > 8);
-
-  int width = 1;
-  int spaces = height - width;
-
-  for (int i = 0; i < height; i++)
-  {
-    for (int z = 1; z <= spaces; z++) // Adding white spaces before dashes
+    int height = 0;
+    do
     {
-        printf(" ");
+        height = get_int("Height: ");
     }
+    while (height < 1 || height > 8);
 
-    for (int j = 1; j <= width; j++) // Adding first set of dashes
-    {
-        printf("#");
-    }
-    
-    for (int s = 0; s < 2; s++) // Adding white spaces between the two sets of dashes
-    {
-        printf(" ");
-    }
-    
-    for (int h = 1; h <= width; h++) // Adding the second set of dashes
-    {
-        printf("#");
-    }
+    int width = 1;
+    int spaces = height - width;
 
-    printf("\n"); // To the new line
-    width++;
-    spaces--;
-  }
+    for (int i = 0; i < height; i++)
+    {
+        for (int z = 1; z <= spaces; z++) // Adding white spaces before dashes
+        {
+                printf(" ");
+        }
+
+        for (int j = 1; j <= width; j++) // Adding first set of dashes
+        {
+                printf("#");
+        }
+        
+        for (int s = 0; s < 2; s++) // Adding white spaces between the two sets of dashes
+        {
+                printf(" ");
+        }
+        
+        for (int h = 1; h <= width; h++) // Adding the second set of dashes
+        {
+                printf("#");
+        }
+
+        printf("\n"); // To the new line
+        width++;
+        spaces--;
+    }
 }
