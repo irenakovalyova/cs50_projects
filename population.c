b94a625ff01e4ed8aacdc5894d5d7f171a8b2d17
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-05 10:33:59 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -3,26 +3,28 @@
 
 int main(void)
 {
-   int start_size;
-  do
-  {
-      start_size = get_int("Start population size: ");
-  }
-  while (start_size < 9);
-
+    int start_size;
+// Get the value of the initial population size from the user
+    do
+    {
+        start_size = get_int("Start population size: ");
+    }
+    while (start_size < 9);
+    
     int end_size;
-  do
-  {
-      end_size = get_int("End population size: ");
-  }
-  while (end_size < start_size);
-
-  int n_years = 0;
-
-  for (n_years = 0; start_size < end_size; n_years++)
-  {
-      start_size = start_size + (start_size / 3) - (start_size / 4);
-  }
+// Get the value of the end population size from the user
+    do
+    {
+        end_size = get_int("End population size: ");
+    }
+    while (end_size < start_size);
+// Calculate the number of years to reach the end size    
+    int n_years = 0;
+    
+    for (n_years = 0; start_size < end_size; n_years++)
+    {
+        start_size = start_size + (start_size / 3) - (start_size / 4);
+    }
     printf("Start size: %i\n", start_size);
     printf("End size: %i\n", end_size);
     printf("Years: %i\n", n_years);
