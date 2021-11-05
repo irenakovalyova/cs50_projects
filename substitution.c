1901f5e7827c2e67379d4b5b031c4c61d0fcd871
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-05 10:03:23 +0000

    automated commit by submit50

--- substitution.c
+++ substitution.c
@@ -7,11 +7,13 @@ string encryptor(string cyphr, string text);
 
 int main(int argc, string argv[])
 {
+// Checking if the key provided doesn't contain white spaces
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
+// Checking if the key provided contains exactly 26 symbols
     else if (strlen(argv[1]) != 26)
     {
         printf("Key must contain 26 characters.\n");
@@ -19,26 +21,28 @@ int main(int argc, string argv[])
     }
     for (int i = 0; i < (strlen(argv[1])); i++)
     {
+// Checking if the key contains only alphabetic symbols
         if (!isalpha(argv[1][i]))
         {
             printf("Invalid key: only alphabetic symbols are allowed\n");
             return 1;
         }
-        
+// Checking if the key contains duplicated symbols        
         for (int j = 0; j != i; j++)
         {
             if (argv[1][i] == argv[1][j] || argv[1][i] == tolower(argv[1][j]) || argv[1][i] == toupper(argv[1][j]))
             {
-                printf("Invalid key: duplicated symbols are not allo0wed\n");
+                printf("Invalid key: duplicated symbols are not allowed\n");
                 return 1;
             }
         }
     }
-    
+// Encoding the provided plain text    
     string plaintext = get_string("plaintext: ");
     printf("ciphertext: %s\n", encryptor(argv[1], plaintext));
 }
 
+// Encodes the provided plain text according to the provided key
 string encryptor(string cyphr, string text)
 {
     for (int i = 0; text[i] != '\0'; i++)
