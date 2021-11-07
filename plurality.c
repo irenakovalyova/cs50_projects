ee9f29df5634cae324b22b0c53898d4e1ac37017
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-07 21:22:10 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -66,9 +66,10 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    int zero = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name))
+        if (strcmp(name, candidates[i].name) == zero)
         {
             candidates[i].votes++;
             return true;
