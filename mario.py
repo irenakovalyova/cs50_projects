84b77afcfd1a530503319f5e55f68812ca41b006
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-15 16:52:55 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -4,23 +4,23 @@ height = 0
 
 while True:
     height = get_int("Height: ")
-    if height > 1 and height < 9:
+    if height > 0 and height < 9:
         break
 
 width = 1
 spaces = height - width
 
 for i in range(0, height, 1):
-    for j in range(1, spaces, 1):
+    for j in range(0, spaces, 1):
         print(" ", end = '')
         
-    for z in range (1, width, 1):
+    for z in range (0, width, 1):
         print("#", end = '')
         
-    for s in range (0, 1, 1):
+    for s in range (0, 2, 1):
         print(" ", end = '')
         
-    for h in range(1, width, 1):
+    for h in range(0, width, 1):
         print("#", end = '')
     
     print('\n')
