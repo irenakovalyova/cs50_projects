a2229a00c0fc440156eeab8a6a99197bffbe6884
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-18 15:26:25 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name='Johnny Depp') AND person_id IN (SELECT id FROM people WHERE name='Helena Bonham Carter'));
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name='Johnny Depp')) AND ID IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name='Helena Bonham Carter'));
