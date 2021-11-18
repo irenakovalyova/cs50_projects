c4c36305743d9303cdd181736d49a7607956cdf6
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-18 15:22:08 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -1,5 +1,5 @@
-SELECT TOP 5 movies.title
+SELECT movies.title
 FROM movies 
 INNER JOIN ratings ON movies.id=ratings.movie_id
 WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name='Chadwick Boseman')
-ORDER BY rating DESC;
+ORDER BY rating DESC
