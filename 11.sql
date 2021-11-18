9baca8a80bf7ba3f48841c41a02a553258e4177f
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-18 15:52:51 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -3,6 +3,6 @@ FROM movies
 JOIN stars ON movies.id=stars.movie_id
 JOIN people ON stars.person_id=people.id
 JOIN ratings ON movies.id=ratings.movie_id
-WHERE people.name='Kevin Bacon' AND people.birth=1958
+WHERE people.name='Chadwick Boseman'
 ORDER BY ratings.rating DESC
 LIMIT 5;
