fd57c63b572bb14f99514b78d9e1085f093db9d5
irenakovalyova <irenakovalyova@users.noreply.github.com>
2021-11-18 14:51:14 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,5 +1,5 @@
 SELECT movies.title, ratings.rating
 FROM movies
 INNER JOIN ratings ON movies.id=ratings.movie_id
-WHERE movies.year=
-ORDER BY ratings.rating DESC, movies.title ASC;
+WHERE movies.year=2010
+ORDER BY rating DESC, title ASC;
